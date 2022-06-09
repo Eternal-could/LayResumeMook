8cd43873827d49028a214bafcd20fe69a1d2fcce
Eternal-could <20578466@qq.com>
2022-06-09 22:13:07 +0800

    修改注释

--- webpack/webpack.render.dev.js
+++ webpack/webpack.render.dev.js
@@ -9,7 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const devConfig = {
   mode: 'development',
   entry: {
-    // 👇 对应渲染进程的 app.jsx 入口文件
+    // 👇 对应渲染进程的 app.tsx 入口文件
     index: path.resolve(__dirname, '../app/render/app.tsx')
   },
   output: {
