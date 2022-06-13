85e6b5551486a3402c273fb9cc699d6bc7bbafac
Eternal-could <20578466@qq.com>
2022-06-13 20:47:52 +0800

    简历模块主入口

--- app/render/container/root/index.tsx
+++ app/render/container/root/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import './index.less'
+import Logo from '../../../../assets/logo.png';
 
 const Root = () => {
   return (
-    <div>我是首页哦</div>
+    <div>
+      <div>
+        <img src={Logo} alt=""/>
+        <div>LayResumeMook</div>
+        <div>一个简单的模板简历制作平台, 更容易实现一个完整的简历 ~</div>
+        <div>
+          {
+            ['介绍', '简历', '源码'].map( (text, index) => {
+              return (
+                <div key={index}>{text}</div>
+              )
+            })
+          }
+        </div>
+        <div>
+          <div>
+            <p>
+              Copyright &copy; 2022-{new Date().getFullYear()} All Rights Reserved. Copyright By LeiYongJiu
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
