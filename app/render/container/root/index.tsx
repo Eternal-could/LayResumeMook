97f8c98bdb3c7d258fe9c10fda16497604f197db
Eternal-could <20578466@qq.com>
2022-06-13 21:12:47 +0800

    跟路由css

--- app/render/container/root/index.tsx
+++ app/render/container/root/index.tsx
@@ -4,23 +4,23 @@ import Logo from '../../../../assets/logo.png';
 
 const Root = () => {
   return (
-    <div>
-      <div>
+    <div styleName="root">
+      <div styleName="container">
         <img src={Logo} alt=""/>
-        <div>LayResumeMook</div>
-        <div>一个简单的模板简历制作平台, 更容易实现一个完整的简历 ~</div>
-        <div>
+        <div styleName="title">LayResumeMook</div>
+        <div styleName="tips">一个简单的模板简历制作平台, 更容易实现一个完整的简历 ~</div>
+        <div styleName="action">
           {
             ['介绍', '简历', '源码'].map( (text, index) => {
               return (
-                <div key={index}>{text}</div>
+                <div key={index} styleName="item">{text}</div>
               )
             })
           }
         </div>
-        <div>
-          <div>
-            <p>
+        <div styleName="copyright">
+          <div styleName="footer">
+            <p styleName="copyright">
               Copyright &copy; 2022-{new Date().getFullYear()} All Rights Reserved. Copyright By LeiYongJiu
             </p>
           </div>
