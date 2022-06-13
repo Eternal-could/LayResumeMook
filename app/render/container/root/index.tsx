7178f4bfbceae521d4700afccc757c6301fef2ae
Eternal-could <20578466@qq.com>
2022-06-13 21:50:30 +0800

    简历模块及路由

--- app/render/container/root/index.tsx
+++ app/render/container/root/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './index.less'
+import { useHistory } from 'react-router'
 import Logo from '../../../../assets/logo.png';
 
 const Root = () => {
+  // 通过 history.push 进行跳转
+  const history = useHistory()
+  const onRouterToLink = (text: string) => {
+    if (text === '简历') {
+      console.log('跳转到简历页面')
+      history.push('/resume')
+    } else {
+      console.log('进入到 github')
+    }
+  }
   return (
     <div styleName="root">
       <div styleName="container">
@@ -13,7 +24,11 @@ const Root = () => {
           {
             ['介绍', '简历', '源码'].map( (text, index) => {
               return (
-                <div key={index} styleName="item">{text}</div>
+                <div
+                  key={index}
+                  styleName="item"
+                  onClick={()=>{onRouterToLink(text)}}
+                >{text}</div>
               )
             })
           }
--- app/render/router.tsx
+++ app/render/router.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Root from './container/root';
+import Resume from './container/resume';
 
 const Router = () => {
   return (
@@ -8,6 +9,9 @@ const Router = () => {
       <Switch>
         <Route path="/" exact>
           <Root />
+        </Route>
+        <Route path="/resume" exact>
+          <Resume/>
         </Route>
       </Switch>
       {/*重定向到首页*/}
